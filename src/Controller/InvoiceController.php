dd543dd532ccfd0166102519481847ea9261544f
tdutrion <thomas@engineor.com>
2019-05-22 23:31:56 +0200

    Add invoice CRUD

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use App\Form\InvoiceType;
+use App\Repository\InvoiceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/invoice")
+ */
+class InvoiceController extends AbstractController
+{
+    /**
+     * @Route("/", name="invoice_index", methods={"GET"})
+     */
+    public function index(InvoiceRepository $invoiceRepository): Response
+    {
+        return $this->render('invoice/index.html.twig', [
+            'invoices' => $invoiceRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="invoice_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $invoice = new Invoice();
+        $form = $this->createForm(InvoiceType::class, $invoice);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($invoice);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('invoice_index');
+        }
+
+        return $this->render('invoice/new.html.twig', [
+            'invoice' => $invoice,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="invoice_show", methods={"GET"})
+     */
+    public function show(Invoice $invoice): Response
+    {
+        return $this->render('invoice/show.html.twig', [
+            'invoice' => $invoice,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="invoice_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Invoice $invoice): Response
+    {
+        $form = $this->createForm(InvoiceType::class, $invoice);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('invoice_index', [
+                'id' => $invoice->getId(),
+            ]);
+        }
+
+        return $this->render('invoice/edit.html.twig', [
+            'invoice' => $invoice,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="invoice_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Invoice $invoice): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$invoice->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($invoice);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('invoice_index');
+    }
+}
